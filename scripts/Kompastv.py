bb3ee931489d41682e8d09ae277fc8eca3b584e4
fhuzhienk <61088819+fhuzhienk@users.noreply.github.com>
2024-04-23 00:06:46 +0700

    Update and rename AlQuranKareem.py to Kompastv.py

--- scripts/Kompastv.py
+++ scripts/Kompastv.py
@@ -40,7 +40,7 @@ print('#EXTM3U')
 print('#EXT-X-VERSION:3')
 print('#EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=1487000,FRAME-RATE=30')
 s = requests.Session()
-with open('../AlQuranKareem_info.txt') as f:
+with open('../KompasTV_info.txt') as f:
     for line in f:
         line = line.strip()
         if not line or line.startswith('~~'):
